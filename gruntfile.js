e781bf092b028a85d1aabb06ee03a899d36d0ac4
Carlos Avila <caravila@cobogpgp1107.local>
2015-06-16 22:53:04 -0500

    add jasmine dependency

--- gruntfile.js
+++ gruntfile.js
@@ -13,11 +13,22 @@ module.exports = function(grunt) {
                 "ext": "html",
                 "runInBackground": false
             }
+        },
+        "jasmine": {
+            "customTemplate": {
+                "src": 'scripts/app/**/*.js',
+                "options": {
+                    "specs": 'spec/*Spec.js',
+                    "template": 'custom.tmpl'
+                }
+            }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
     grunt.registerTask('default', ['http-server']);
+    grunt.registerTask('jasmine', ['jasmine']);
 };
