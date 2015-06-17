1e38d91a16ec750fbbd211aa1c69348642a9f372
Carlos Avila <caravila@cobogpgp1107.local>
2015-06-17 16:07:38 -0500

    watch functionality for grunt

---
+++ scripts/app/views/tests/appView-spec.js
@@ -0,0 +1,5 @@
+describe("TODO list app view", function() {
+    it("should be a true value", function() {
+        expect(true).toBe(true);
+    });
+});
---
+++ specs/app/collections/todoCollection-spec.js
@@ -0,0 +1,8 @@
+'use strict';
+define(['scripts/app/collections/todoCollection'], function(todoCollection) {
+	describe('todoCollection', function(){
+		it('should be true', function(){
+			expect(true).toBe(true);
+		});
+	});
+});
